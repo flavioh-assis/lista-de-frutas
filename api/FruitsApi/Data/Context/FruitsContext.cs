7dedc4b9527641891b6cc0775c2b5cc6112f083a
Flavio Assis <flavioh.dev@gmail.com>
2023-02-25 18:47:05 -0300

    Cria contexto

---
+++ api/FruitsApi/Data/Context/FruitsContext.cs
@@ -0,0 +1,15 @@
+using FruitsApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FruitsApi.Data.Context
+{
+    public class FruitContext : DbContext
+    {
+        public FruitContext(DbContextOptions<FruitContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Fruit> Fruits { get; set; }
+    }
+}
