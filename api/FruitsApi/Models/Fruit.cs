2c4aa164c172619f7ed922bc5cfa0ecd7c4bb231
Flavio Assis <flavioh.dev@gmail.com>
2023-02-25 18:46:31 -0300

    Create model Fruit

---
+++ api/FruitsApi/Models/Fruit.cs
@@ -0,0 +1,9 @@
+namespace FruitsApi.Models;
+
+public class Fruit
+{
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public int ValueA { get; set; }
+    public int ValueB { get; set; }
+}
