f660b70f9d942176c7eedd0a3c8f9b2ce5881e59
Flavio Assis <flavioh.dev@gmail.com>
2023-02-27 14:35:11 -0300

    Cria projeto do front

---
+++ front/.prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  useTabs: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "avoid",
+  endOfLine: "crlf",
+  singleAttributePerLine: true,
+};
---
+++ front/src/routes.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route
+        path='/'
+        element
+      />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
