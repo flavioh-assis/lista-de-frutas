e50f98e73a56602d551d51524ade376634d42d72
Flavio Assis <flavioh.dev@gmail.com>
2023-02-28 16:18:00 -0300

    Ajusta padding

--- front/src/styles/shared.ts
+++ front/src/styles/shared.ts
@@ -4,7 +4,7 @@ export const Page = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 5rem;
+  padding: 5rem 10rem;
   background: lightGrey;
   min-height: 100vh;
 `;
