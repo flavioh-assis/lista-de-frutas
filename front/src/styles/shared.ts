33876fdf08ad3fafdc41d67971a4af2e1e8bdf74
Flavio Assis <flavioh.dev@gmail.com>
2023-02-28 13:21:38 -0300

    Cria styled de Page

---
+++ front/src/styles/shared.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Page = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 5rem;
+  background: lightGrey;
+  min-height: 100vh;
+`;
+
