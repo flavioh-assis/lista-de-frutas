d825df1cdc54a690ba871b4712d6e35b87bd4fa4
Flavio Assis <flavioh.dev@gmail.com>
2023-02-28 12:59:03 -0300

    Cria rotas

--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom';
+import { AddFruit } from './pages';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route
         path='/'
-        element
+        element={<AddFruit />}
       />
     </Switch>
   </BrowserRouter>
 );
 
 export default Routes;
+
