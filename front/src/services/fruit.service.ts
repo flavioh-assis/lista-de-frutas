246c731264ec8fab9a5d74a0748bed5c7b384745
Flavio Assis <flavioh.dev@gmail.com>
2023-03-01 16:59:36 -0300

    Cria query

--- front/src/services/fruit.service.ts
+++ front/src/services/fruit.service.ts
@@ -1,12 +1,26 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import api from './api';
 import { Fruit, FruitDTO } from '../types';
 
 const create = async (fruit: FruitDTO) => {
-  const { data } = await api.post('FruitRequests', fruit);
+  const { data } = await api.post('fruits', fruit);
 
   return data as Fruit;
 };
 
+const getAll = async () => {
+  const { data } = await api.get('fruits');
+
+  return data ? (data as Fruit[]) : [];
+};
+
 export const MutationCreate = () => useMutation(create);
+
+export const UseQueryAllFruits = () => {
+  return useQuery(['getAll'], () => getAll(), {
+    enabled: false,
+    retryDelay: 5000,
+    retry: 5,
+  });
+};
 
