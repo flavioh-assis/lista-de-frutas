b774690dff68114510d3291f498af88006ad5e59
Flavio Assis <flavioh.dev@gmail.com>
2023-03-01 16:58:50 -0300

    Limita largura e remove background

--- front/src/styles/shared.ts
+++ front/src/styles/shared.ts
@@ -4,8 +4,11 @@ export const Page = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 5rem 10rem;
-  background: lightGrey;
+  padding: 5rem 2rem;
+
+  max-width: 92rem;
   min-height: 100vh;
+  margin: auto;
+  gap: 5rem;
 `;
 
