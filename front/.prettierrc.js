674c0dd0eb09eb3b803b62a4539080e216b03963
Flavio Assis <flavioh.dev@gmail.com>
2023-03-01 21:49:07 -0300

    Ajsuta EoL

--- front/.prettierrc.js
+++ front/.prettierrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-  trailingComma: "es5",
+  trailingComma: 'es5',
   tabWidth: 2,
   semi: true,
   singleQuote: true,
   printWidth: 100,
   useTabs: false,
-  quoteProps: "as-needed",
+  quoteProps: 'as-needed',
   jsxSingleQuote: true,
   bracketSpacing: true,
   bracketSameLine: false,
-  arrowParens: "avoid",
-  endOfLine: "crlf",
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
   singleAttributePerLine: true,
 };
+
