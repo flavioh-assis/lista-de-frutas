13c5c5d7f6032c13295a3d31d35a26dfb9a9237d
Flavio Assis <flavioh.dev@gmail.com>
2023-03-01 21:55:31 -0300

    Consome o novo Form

--- front/src/pages/AddFruit/AddFruit.tsx
+++ front/src/pages/AddFruit/AddFruit.tsx
@@ -1,12 +1,11 @@
-import { Form, notification } from 'antd';
+import { notification } from 'antd';
 import { useEffect } from 'react';
-import { Button, Input, Table } from '../../components';
+import { FormFruit, Table } from '../../components';
 import { MutationCreate, UseQueryAllFruits } from '../../services/fruit.service';
 import { Page as PageAddFruit } from '../../styles/shared';
-import { FruitDTO } from '../../types';
+import { Fruit, FruitDTO } from '../../types';
 
 const AddFruit = () => {
-  const [formFruit] = Form.useForm<FruitDTO>();
   const [api, contextHolder] = notification.useNotification();
 
   const { mutate } = MutationCreate();
@@ -33,6 +32,22 @@ const AddFruit = () => {
     });
   };
 
+  const handleSubmit = async (fruit: Fruit) => {
+    return new Promise<void>((resolve, rej) => {
+      mutate(fruit, {
+        onSuccess: () => {
+          openNotificationSuccess(fruit);
+          refetch();
+          resolve();
+        },
+        onError: error => {
+          openNotificationError(error);
+          rej();
+        },
+      });
+    });
+  };
+
   useEffect(() => {
     refetch();
   }, []);
@@ -41,68 +56,7 @@ const AddFruit = () => {
     <PageAddFruit>
       {contextHolder}
 
-      <Form
-        style={{ alignItems: 'flex-end' /* , maxWidth: '70rem' */ }}
-        form={formFruit}
-        onFinish={newFruit =>
-          mutate(newFruit, {
-            onSuccess: () => openNotificationSuccess(newFruit),
-            onError: error => openNotificationError(error),
-          })
-        }
-        validateMessages={{
-          required: 'Campo obrigatório',
-        }}
-        validateTrigger={false}
-        layout='inline'
-      >
-        <Form.Item
-          label='Descrição'
-          labelAlign='left'
-          labelCol={{ span: 20 }}
-          wrapperCol={{ span: 20 }}
-          name='description'
-          rules={[{ required: true }]}
-          style={{ marginInlineEnd: 0 }}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label='Valor A'
-          labelAlign='left'
-          labelCol={{ span: 20 }}
-          wrapperCol={{ span: 20 }}
-          name='valueA'
-          rules={[{ required: true }]}
-          style={{ marginInlineEnd: 0 }}
-        >
-          <Input type='number' />
-        </Form.Item>
-
-        <Form.Item
-          label='Valor B'
-          labelCol={{ span: 20 }}
-          wrapperCol={{ span: 20 }}
-          name='valueB'
-          labelAlign='left'
-          rules={[{ required: true }]}
-          style={{ marginInlineEnd: 0 }}
-        >
-          <Input type='number' />
-        </Form.Item>
-
-        <div style={{ display: 'flex', gap: '2rem' }}>
-          <Button
-            type='submit'
-            color='primary'
-          >
-            Adicionar
-          </Button>
-
-          <Button type='reset'>Limpar</Button>
-        </div>
-      </Form>
+      <FormFruit onSubmit={handleSubmit} />
 
       <Table fruits={fruits} />
     </PageAddFruit>
@@ -110,4 +64,3 @@ const AddFruit = () => {
 };
 
 export default AddFruit;
-
