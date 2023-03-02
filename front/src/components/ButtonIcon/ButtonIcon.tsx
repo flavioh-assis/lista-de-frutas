688c951b6af8b37f7530484d9c2320d88ee81972
Flavio Assis <flavioh.dev@gmail.com>
2023-03-02 12:43:39 -0300

    Implementa onClick

--- front/src/components/ButtonIcon/ButtonIcon.tsx
+++ front/src/components/ButtonIcon/ButtonIcon.tsx
@@ -4,10 +4,11 @@ import { StyledButton } from './ButtonIcon.styled';
 type Props = {
   icon: 'link' | 'edit' | 'delete';
   size?: number;
+  onClick: VoidFunction;
 };
 
-const ButtonIcon = ({ icon, size = 20 }: Props) => {
-  const iconComponent = ({ icon, size }: Props) => {
+const ButtonIcon = ({ icon, size = 20, onClick }: Props) => {
+  const iconComponent = (icon: string, size: number) => {
     if (icon === 'link') {
       return <FiExternalLink size={size} />;
     }
@@ -17,8 +18,14 @@ const ButtonIcon = ({ icon, size = 20 }: Props) => {
     return <FiTrash2 size={size} />;
   };
 
-  return <StyledButton>{iconComponent({ icon, size })}</StyledButton>;
+  return (
+    <StyledButton
+      onClick={() => onClick()}
+      type='button'
+    >
+      {iconComponent(icon, size)}
+    </StyledButton>
+  );
 };
 
 export default ButtonIcon;
-
