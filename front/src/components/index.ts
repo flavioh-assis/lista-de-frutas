8ebf94014d3495271756b8c3e7ccd6b90073516a
Flavio Assis <flavioh.dev@gmail.com>
2023-03-02 17:44:01 -0300

    Add export

--- front/src/components/index.ts
+++ front/src/components/index.ts
@@ -2,6 +2,7 @@ import Button from './Button/Button';
 import ButtonIcon from './ButtonIcon/ButtonIcon';
 import FormFruit from './Form/Form';
 import FormInput from './FormInput/FormInput';
+import Modal from './Modal/Modal';
 import Table from './Table/Table';
 
-export { Button, ButtonIcon, FormFruit, FormInput, Table };
+export { Button, ButtonIcon, FormFruit, FormInput, Modal, Table };
