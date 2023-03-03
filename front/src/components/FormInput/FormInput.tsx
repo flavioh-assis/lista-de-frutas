768a383c929433809f594c67f19e03641d2d8d1e
Flavio Assis <flavioh.dev@gmail.com>
2023-03-02 22:57:42 -0300

    Add disabled prop

--- front/src/components/FormInput/FormInput.tsx
+++ front/src/components/FormInput/FormInput.tsx
@@ -5,15 +5,17 @@ type Props = {
   label: string;
   register: UseFormRegisterReturn;
   type?: React.HTMLInputTypeAttribute;
+  disabled?: boolean;
 };
 
-const FormInput = ({ label, register, type = 'string' }: Props) => {
+const FormInput = ({ label, register, type = 'string', disabled = false }: Props) => {
   return (
     <Wrapper>
       <label>{label}</label>
       <Input
         {...register}
         type={type}
+        disabled={disabled}
       />
     </Wrapper>
   );
