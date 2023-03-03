09036e1ef1838f74eae1386e88d27079b34b3c00
Flavio Assis <flavioh.dev@gmail.com>
2023-03-03 12:33:46 -0300

    Indentação

--- api/FruitsApi/Controllers/FruitsController.cs
+++ api/FruitsApi/Controllers/FruitsController.cs
@@ -13,85 +13,85 @@ public class FruitsController : ControllerBase
     private readonly FruitsService _service;
     private readonly IMapper _mapper;
 
-    public FruitsController(FruitsService service, IMapper mapper) {
+    public FruitsController(FruitsService service, IMapper mapper)
+    {
         _service = service;
         _mapper = mapper;
     }
 
-[HttpPost]
-public async Task<ActionResult<Fruit>> Post([FromBody] FruitDto dto)
-{
-    try
+    [HttpPost]
+    public async Task<ActionResult<Fruit>> Post([FromBody] FruitDto dto)
     {
-        var fruit = _mapper.Map<Fruit>(dto);
-        var createdFruit = await _service.Create(fruit);
+        try
+        {
+            var fruit = _mapper.Map<Fruit>(dto);
+            var createdFruit = await _service.Create(fruit);
 
-        return Created("Fruit created.", createdFruit);
+            return Created("Fruit created.", createdFruit);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+        }
     }
-    catch (Exception e)
+
+    [HttpGet]
+    public async Task<ActionResult<List<Fruit>>> GetAll()
     {
-        return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
-    }
-}
+        try
+        {
+            var fruits = await _service.GetAll();
 
-[HttpGet]
-public async Task<ActionResult<List<Fruit>>> GetAll()
-{
-    try
+            return Ok(fruits);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+        }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Fruit>> GetById([FromRoute] int id)
     {
-        var fruits = await _service.GetAll();
+        try
+        {
+            var fruit = await _service.GetById(id);
 
-        return Ok(fruits);
+            return Ok(fruit);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+        }
     }
-    catch (Exception e)
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Fruit>> Update([FromRoute] int id, [FromBody] FruitDto dto)
     {
-        return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
-    }
-}
+        try
+        {
+            var fruit = await _service.Update(id, dto);
 
-[HttpGet("{id}")]
-public async Task<ActionResult<Fruit>> GetById([FromRoute] int id)
-{
-    try
+            return Ok(fruit);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Fruit>> Delete([FromRoute] int id)
     {
-        var fruit = await _service.GetById(id);
+        try
+        {
+            var deletedFruit = await _service.Delete(id);
 
-        return Ok(fruit);
+            return Ok(deletedFruit);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+        }
     }
-    catch (Exception e)
-    {
-        return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
-    }
-}
-
-[HttpPut("{id}")]
-public async Task<ActionResult<Fruit>> Update([FromRoute] int id, [FromBody] FruitDto dto)
-{
-    try
-    {
-        var fruit = await _service.Update(id, dto);
-
-        return Ok(fruit);
-    }
-    catch (Exception e)
-    {
-        return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
-    }
-}
-
-[HttpDelete("{id}")]
-public async Task<ActionResult<Fruit>> Delete([FromRoute] int id)
-{
-    try
-    {
-        var deletedFruit = await _service.Delete(id);
-
-        return Ok(deletedFruit);
-    }
-    catch (Exception e)
-    {
-        return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
-    }
-}
-
 }
