3898490d0789ceb623793cf2e8ee02903a566654
Flavio Assis <flavioh.dev@gmail.com>
2023-03-03 19:38:56 -0300

    Limpa comments

--- front/src/styles/globals.ts
+++ front/src/styles/globals.ts
@@ -1,6 +1,4 @@
 import { createGlobalStyle } from 'styled-components';
-// import colors from 'resources/colors';
-// import fonts from 'resources/fonts';
 
 export default createGlobalStyle`
   #root {
@@ -16,20 +14,3 @@ export default createGlobalStyle`
 		font-family: 'Roboto';
   }
 `;
-
-/* color: ${colors.text}; */
-
-/* font-family: Roboto; */
-/* @font-face {
-      font-family: "SF Reg";
-      src: url(
-        'https://applesocial.s3.amazonaws.com/assets/styles/fonts/sanfrancisco/sanfranciscodisplay-regular-webfont.woff'
-      );
-    } */
-
-/* @font-face {
-      font-family: "SF Bold";
-      src: url(
-        'https://applesocial.s3.amazonaws.com/assets/styles/fonts/sanfrancisco/sanfranciscodisplay-bold-webfont.woff'
-      );
-    } */
