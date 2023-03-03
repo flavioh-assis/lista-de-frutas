55af64be974585496398d0bc59173f4f23e72e27
Flavio Assis <flavioh.dev@gmail.com>
2023-03-02 23:00:26 -0300

    Cria rota para novo pagina

--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom';
 import { AddFruit } from './pages';
+import Calculate from './pages/Calculate/Calculate';
 
 const Routes = () => (
   <BrowserRouter>
@@ -8,9 +9,12 @@ const Routes = () => (
         path='/'
         element={<AddFruit />}
       />
+      <Route
+        path='/calculate'
+        element={<Calculate />}
+      />
     </Switch>
   </BrowserRouter>
 );
 
 export default Routes;
-
